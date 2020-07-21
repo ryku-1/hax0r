5ba794c3ef3756a2154e2a5ff4f67e82b0621fc5
ryku <62566476+ryku-1@users.noreply.github.com>
2020-07-21 16:06:59 +0100

    Update dnzt.py

--- python/dnzt.py
+++ python/dnzt.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!usr/bin/pyton3
 # encoding: utf-8
 
 import dns.query
@@ -6,12 +6,21 @@ import dns.zone
 import dns.resolver
 import sys
 
+print(25 * '#')
+print(25 * '-')  
+print('A tool for Dns Zone Transfers')
+print('by ryku')
+print(25 * '-') 
+print(25 * '#')
+
+
 try :
 
-    z = dns.zone.from_xfr(dns.query.xfr(sys.argv[1] , sys.argv[2]))
-    names = z.nodes.keys()
-    for n in names:
-        print(z[n].to_text(n))
+	z = dns.zone.from_xfr(dns.query.xfr(sys.argv[1] , sys.argv[2]))
+	names = z.nodes.keys()
+	for n in names:
+		print(z[n].to_text(n))
 
 except IndexError:
-    print('usage: python3 dnzt.py <ip> <host>')
+	print(25 * '-')
+	print('usage: python3 dnzt.py <ip> <host>')
