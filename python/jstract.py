ea3a7fcde0c13c08327585126bbf5beb4f1390fb
ryku <62566476+ryku-1@users.noreply.github.com>
2020-07-21 13:54:29 +0100

    Create jstract.py

---
+++ python/jstract.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+
+import re
+import sys
+
+filenames = set()
+
+with open(sys.argv[-1]) as i:
+        for line in i:
+                end = line.rfind(".js") + 3
+                start = line.rfind("/", 0, end) + 1
+                filename = line[start:end]
+                if filename.endswith(".js"):
+                        filenames.add(filename)
+
+
+for filename in sorted(filenames, key=str.lower):
+        print(filename)
